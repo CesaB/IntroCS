8b0d77bea9d412598974c8a2887e652a46ad6d41
CesaB <18cburgos@iaa.edu.ec>
2015-05-07 22:38:37 -0500

    Create ClassWork.py

---
+++ ClassWork.py
@@ -0,0 +1,17 @@
+mystring = "Hello" #string 
+myfloat = 10 #floatingpoint
+myint = 20 #interger
+print
+#Printing on Screen
+print mystring 
+print myfloat
+print myint
+print 
+print mystring, ",I was ", myfloat, "years old", ",I should be", myint, "in more than ten years"
+print 
+#Mathematical operations
+print 5*12/3
+print (6+3)*2
+print 60/4*2
+print 8+6-2
+print 4*9*3

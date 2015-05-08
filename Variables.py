2047a2e230eceb8f1f8e2d41aeff7d6ebb29e60e
CesaB <18cburgos@iaa.edu.ec>
2015-05-07 22:39:45 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,24 @@
+name = "Cesar Burgos" #string 
+age = 14 #integer
+gpa = 4.0 #floating point
+
+print name
+print age 
+print gpa
+
+print 'My name is', name,'. I am', age, 'years old. My gpa is', gpa
+
+print 'in then years, I will be', age + 10 
+
+#Dog years
+ageInHumanyears = 3
+ageInDogYears = ageInHumanYears * 7
+
+print 'My dog is', ageInDogYears, 'years old in Dog Years'
+print 'But my dog is', ageInHumanYears, 'years old in Human Years.'
+
+#Order of operations
+#Parenthesis, Exponents, Multiplication, Division, Aaddition, Substraction
+myNumber = 5 * 4 + 10
+print myNumber
+print "Hello" + 2

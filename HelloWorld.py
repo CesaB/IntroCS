54c40613897af72ab8b3fb7b7b70ddd83b75cad2
CesaB <18cburgos@iaa.edu.ec>
2015-05-07 22:29:32 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print "HelloWorld"
+print "My name is Cesar"
+print "I am 14 years old \nthis is python"
+
+print 2015 + 1 #Printing an interger
+print "2015"
+
+#Math Operations
+print 3.1416 #Printing a floating point number
+
+print 2018 + 7 #adding two numbers
+
+print 2016 - 2 #substracting two numbers
+
+print 2017 * 4 #Multiplying two numbers 
+
+#Modulus
+print 10%3
+
+print "Odd or Even Numbers"
+print 17 % 3

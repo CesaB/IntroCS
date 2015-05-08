c7ab5f6ef0e14465bf7708e67c4be30668640e2f
CesaB <18cburgos@iaa.edu.ec>
2015-05-07 22:37:57 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,17 @@
+name = "Cesar" #string 
+lastname = "Burgos" 
+adress = "Cdl. Ciudad Celeste" 
+email = "18cburgos@iaa.edu.ec" 
+phonenumber = "0997576671" 
+
+print name 
+print lastname 
+print adress
+print email 
+print phonenumber 
+
+print #this creates a space between each print
+print #this creates a space between each print
+
+#Printing variables all together in one line
+print "My name is", Name, "and my last name is", Last, "I live in", Addres, "my email is", Email, "and my phone number is", Phone, "nice to meet you"

1ade323e6aefceea848ae4fe5074967f2888acbf
CesaB <18cburgos@iaa.edu.ec>
2015-05-20 09:54:05 -0500

    Create Homework1_1.py

---
+++ Homework1_1.py
@@ -0,0 +1,13 @@
+#name = "Cesar"
+#print "My name is", name
+
+import sys
+print "Your name is: ", sys.argv[1]
+print "Your middle name is: ", sys.argv[2]
+print "Your last name is: ", sys.argv[3]
+
+#name = "Cesar"
+#Lastname = "Burgos"
+#Yourofbirth = "2000"
+#Yourofgraduation = "2018"
+#YourGPA = "5"
